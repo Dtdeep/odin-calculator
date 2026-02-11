38be568d1f2aeb519b5053614b386655244f42e9
Dtdeep <dumagandesthiny@gmail.com>
2026-02-11 17:24:45 +0800

    Create Four functions in Calculator add,subtract, divide, multiply
    receives two numbers and operates on them

---
+++ calculator.js
@@ -0,0 +1,15 @@
+const add  = (addendOne, addendTwo) => {
+    return addendOne + addendTwo
+}
+
+const subtract = (minuend, subrahend) =>{
+    return minuend - subrahend;
+}
+
+const multiply = (multiplicand, multiplier) => {
+    return multiplicand * multiplier;
+}
+
+const divide = (dividend, divisor) =>{
+    return dividend / divisor;
+}
