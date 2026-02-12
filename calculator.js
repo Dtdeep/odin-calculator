13eaf7703d1c3102e9df7740f94f2500e2e426a9
Dtdeep <dumagandesthiny@gmail.com>
2026-02-12 12:21:06 +0800

    Create operator function that operates two numbers

--- calculator.js
+++ calculator.js
@@ -1,3 +1,8 @@
+const num1 = 0;
+const num2 = 0;
+const operator = "";
+
+
 const add  = (addendOne, addendTwo) => {
     return addendOne + addendTwo
 }
@@ -12,4 +17,21 @@ const multiply = (multiplicand, multiplier) => {
 
 const divide = (dividend, divisor) =>{
     return dividend / divisor;
+}
+
+const operate = (operator,num1,num2)=>{
+    switch(operator){
+        case "+":
+            add(num1,num2);
+            break;
+        case "-":
+            subtract(num1,num2);
+            break;
+        case "x":
+            multiply(num1,num2);
+            break;
+        case "/":
+            divide(num1,num2);
+            break;
+    }
 }
